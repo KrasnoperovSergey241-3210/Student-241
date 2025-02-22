1dcf06788b503e3324b9ad1f8aee6f70c42dc540
Krasnoperov Sergey <sakrasnoperov2006@gmail.com>
2025-02-22 07:47:19 +0300

    Homework_Done

---
+++ Homework3/Incapsulation/index.php
@@ -0,0 +1,37 @@
+<?php
+class Cat
+
+{
+    private $name;
+    private $color;
+
+    public function __construct(string $name, string $color)
+    {
+        $this->name = $name;
+        $this->color = $color;
+    }
+
+    public function sayHello()
+    {
+        echo 'Привет! Меня зовут ' . $this->name . '. Я '.$this->color.'.';
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+}
+
+$mycat = new Cat('Белка', 'серо-белая в полоску');
+
+echo $mycat->sayHello();
