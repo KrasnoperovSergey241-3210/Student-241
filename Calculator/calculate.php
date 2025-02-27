619566dacbc73eab2a439c7db0f046d06c12ed92
Krasnoperov Sergey <sakrasnoperov2006@gmail.com>
2025-02-27 14:57:35 +0300

    Commit

--- Calculator/calculate.php
+++ Calculator/calculate.php
@@ -1,21 +1,78 @@
 <?php
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Получаем выражение из POST-запроса
     $expression = $_POST['expression'];
 
-    // Проверяем выражение на допустимые символы
-    if (preg_match('/^[0-9+\-*\/()\.\s]+$/', $expression)) {
-        // Вычисляем результат
-        try {
-            eval('$result = ' . $expression . ';');
-            echo $result;
-        } catch (Throwable $e) {
-            echo "Ошибка: Некорректное выражение";
-        }
-    } else {
-        echo "Ошибка: Недопустимые символы в выражении";
+    // Проверка на допустимые символы
+    if (!preg_match('/^[0-9+\-*\/().\s]+$/', $expression)) {
+        echo "Ошибка: Недопустимые символы в выражении.";
+        exit;
     }
-} else {
-    echo "Ошибка: Неверный метод запроса";
+
+    // Вычисление выражения
+    $result = evaluateExpression($expression);
+
+    // Возврат результата
+    echo $result;
 }
-?>
+
+function evaluateExpression($expression) {
+    // Удаляем пробелы
+    $expression = str_replace(' ', '', $expression);
+
+    // Проверяем баланс скобок
+    if (!isBalanced($expression)) {
+        return "Ошибка: Неправильное расположение скобок.";
+    }
+
+    // Вычисляем выражение
+    try {
+        $result = calculate($expression);
+        return $result;
+    } catch (Exception $e) {
+        return "Ошибка: " . $e->getMessage();
+    }
+}
+
+function isBalanced($expression) {
+    $balance = 0;
+    for ($i = 0; $i < strlen($expression); $i++) {
+        if ($expression[$i] === '(') {
+            $balance++;
+        } elseif ($expression[$i] === ')') {
+            $balance--;
+        }
+        if ($balance < 0) {
+            return false;
+        }
+    }
+    return $balance === 0;
+}
+
+function calculate($expression) {
+    // Рекурсивное вычисление выражений в скобках
+    while (preg_match('/\(([^()]+)\)/', $expression, $matches)) {
+        $subResult = calculate($matches[1]);
+        $expression = str_replace($matches[0], $subResult, $expression);
+    }
+
+    // Вычисление умножения и деления
+    while (preg_match('/(\d+\.?\d*)([*\/])(\d+\.?\d*)/', $expression, $matches)) {
+        $a = $matches[1];
+        $op = $matches[2];
+        $b = $matches[3];
+        $result = ($op === '*') ? $a * $b : $a / $b;
+        $expression = str_replace($matches[0], $result, $expression);
+    }
+
+    // Вычисление сложения и вычитания
+    while (preg_match('/(\d+\.?\d*)([+\-])(\d+\.?\d*)/', $expression, $matches)) {
+        $a = $matches[1];
+        $op = $matches[2];
+        $b = $matches[3];
+        $result = ($op === '+') ? $a + $b : $a - $b;
+        $expression = str_replace($matches[0], $result, $expression);
+    }
+
+    return $expression;
+}
